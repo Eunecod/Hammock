cf62aa84f944498ae187daac4cdb9567d2260a49
Eugene <sudakovzhenya02@gmail.com>
2025-03-28 20:39:18 +0700

    [feat] Дополнение функционал для GameObject
    - Методы для работы с иерархией и компонентами в GameObject.

--- core/engine/include/GameObject.h
+++ core/engine/include/GameObject.h
@@ -7,18 +7,33 @@ private:
     glm::vec3 m_Position;
     glm::quat m_Rotation;
 
-    GameObject* m_pParent { nullptr };
-    std::vector<GameObject*> m_pChildren;
+    std::unique_ptr<Derived> m_pParent;
+    std::vector<std::unique_ptr<Derived>> m_pChildren;
 
 public:
     GameObject(void) { }
+    ~GameObject(void) = default;
+
+    void AddComponent(std::unique_ptr<Derived> pChildren) {
+        if (!pChildren->GetParent()) {
+            pChildren->SetParent(static_cast<Derived*>(this));
+            m_pChildren.push_back(std::move(pChildren));
+        }
+    }
+
+    void SetParent(std::unique_ptr<Derived> pParent) {
+        m_pParent = std::move(pParent);
+    }
 
     GameObject* GetParent(void) const {
         return m_pParent;
     }
 
-    const std::vector<GameObject*>& GetChildren(void) const {
-        return m_pChildren;
+    std::vector<Derived*> GetChildren() const {
+        std::vector<Derived*> children;
+        for (const auto& child : m_pChildren)
+            children.push_back(child.get());
+        return children;
     }
 
     void UpdateObject(double dDelta = 0.0) {
@@ -28,5 +43,5 @@ public:
 
 class Blank : public GameObject<Blank> {
 public:
-    void Update(float deltaTime) { }
+    void Update(double deltaTime) { }
 };
