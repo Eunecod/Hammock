1bb2be559db0d42b1e3591d2ec16819ce7a59491
Eugene <sudakovzhenya02@gmail.com>
2025-03-29 15:24:35 +0700

    [feat] Исправление принципа работы игровых объектов
    - Игровой объект сменил тип полиморфизма с статического на абстрактный (CRTP -> AC).
    - Создан макрос для оптимизации пространства и работы с полями класса.

--- core/engine/include/GameObject.h
+++ core/engine/include/GameObject.h
@@ -1,47 +1,21 @@
 #pragma once
-#include <CommonDefines.h>
+#include <IGameObject.h>
 
 
-template <typename Derived> class GameObject {
-private:
-    glm::vec3 m_Position;
-    glm::quat m_Rotation;
+#define OBJECT_PROPERTY(TYPE, NAME, PROPS) \
+        private: \
+            TYPE PROPS; \
+        public: \
+            const TYPE& Get##NAME() const { return PROPS; } \
+            void Set##NAME(const TYPE& value) { PROPS = value; } \
 
-    std::unique_ptr<Derived> m_pParent;
-    std::vector<std::unique_ptr<Derived>> m_pChildren;
+class CGameObject : public IGameObject {
+    OBJECT_PROPERTY(Engine::Vector3D, Position, position);
+    OBJECT_PROPERTY(Engine::Scale3D, Scale, scale);
+    OBJECT_PROPERTY(Engine::Quantarion, Rotation, rotation);
+    OBJECT_PROPERTY(bool, Active, is_active);
+    OBJECT_PROPERTY(bool, Physics, is_physics);
 
 public:
-    GameObject(void) { }
-    ~GameObject(void) = default;
-
-    void AddComponent(std::unique_ptr<Derived> pChildren) {
-        if (!pChildren->GetParent()) {
-            pChildren->SetParent(static_cast<Derived*>(this));
-            m_pChildren.push_back(std::move(pChildren));
-        }
-    }
-
-    void SetParent(std::unique_ptr<Derived> pParent) {
-        m_pParent = std::move(pParent);
-    }
-
-    GameObject* GetParent(void) const {
-        return m_pParent;
-    }
-
-    std::vector<Derived*> GetChildren() const {
-        std::vector<Derived*> children;
-        for (const auto& child : m_pChildren)
-            children.push_back(child.get());
-        return children;
-    }
-
-    void UpdateObject(double dDelta = 0.0) {
-        static_cast<Derived*>(this)->Update(dDelta);
-    }
-};
-
-class Blank : public GameObject<Blank> {
-public:
-    void Update(double deltaTime) { }
+    CGameObject() = default;
 };
