712859bf15e51d6cdab6752befa82b45ee8302da
Eugene <sudakovzhenya02@gmail.com>
2025-03-29 15:22:07 +0700

    [feat] Новые объявления движка
    - Добавленны зависимости glew и chrono
    - Добавленны константы для работы синхронизации движка
    - Пространство Engine, Frame, Timer

--- core/engine/include/CommonDefines.h
+++ core/engine/include/CommonDefines.h
@@ -1,9 +1,32 @@
 #pragma once 
 #include <glm/gtc/quaternion.hpp>
 #include <unordered_map>
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
 #include <functional> 
 #include <cstdint>
 #include <string>
 #include <memory>
+#include <chrono>
+
+
+constexpr const float TARGET_FPS        { 60.f };
+constexpr const float PHYSICS_STEP      { 1.f / TARGET_FPS };
+constexpr const float MAX_DELTA_TIME    { 0.25f };
+
+namespace Engine {
+    using Vector3D      = glm::vec3;
+    using Scale3D       = glm::vec3;
+    using Quantarion    = glm::quat;
+}
+
+namespace Frame {
+    using Window = GLFWwindow;
+}
+
+namespace Timer {
+    using Clock         = std::chrono::high_resolution_clock;
+    using TimePoint     = std::chrono::time_point<Clock>;
+    using Duration      = std::chrono::duration<float>;
+}
