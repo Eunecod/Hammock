ffc8fea06e6407b956d37ac280c07782f2473310
Eugene <sudakovzhenya02@gmail.com>
2025-03-30 11:57:48 +0700

    [feat] Добавлен шаблон для типобезопастности и переработан макрос для создания PROPERTY у игрового объекта

--- core/engine/include/GameObject.h
+++ core/engine/include/GameObject.h
@@ -2,19 +2,28 @@
 #include <IGameObject.h>
 
 
-#define OBJECT_PROPERTY(TYPE, NAME, PROPS) \
+#define OBJECT_PROPERTY(TYPE, NAME, DEFAULT_VALUE) \
         private: \
-            TYPE PROPS; \
+            TProperty<TYPE> _##NAME##_prop{DEFAULT_VALUE}; \
         public: \
-            const TYPE& Get##NAME() const { return PROPS; } \
-            void Set##NAME(const TYPE& value) { PROPS = value; } \
+            const TYPE& Get##NAME() const { return _##NAME##_prop.Get(); } \
+            void Set##NAME(const TYPE& value) { _##NAME##_prop.Set(value); } \
+
+template <typename T> class TProperty {
+private:
+    T property;
+public:
+    explicit TProperty(const T& value = T { }) : property(value) { }
+    const T& Get() const { return property; }
+    void Set(const T& value) { property = value; }
+};
 
 class CGameObject : public IGameObject {
-    OBJECT_PROPERTY(Engine::Vector3D, Position, position);
-    OBJECT_PROPERTY(Engine::Scale3D, Scale, scale);
-    OBJECT_PROPERTY(Engine::Quantarion, Rotation, rotation);
-    OBJECT_PROPERTY(bool, Active, is_active);
-    OBJECT_PROPERTY(bool, Physics, is_physics);
+    OBJECT_PROPERTY(Engine::Vector3D, Position, Engine::Vector3D(0.f, 0.f, 0.f));
+    OBJECT_PROPERTY(Engine::Vector3D, Scale, Engine::Vector3D(0.f, 0.f, 0.f));
+    OBJECT_PROPERTY(Engine::Quantarion, Rotation, Engine::Quantarion(1.f, 0.f, 0.f, 0.f));
+    OBJECT_PROPERTY(bool, Active, true);
+    OBJECT_PROPERTY(bool, Physics, true);
 
 public:
     CGameObject() = default;
