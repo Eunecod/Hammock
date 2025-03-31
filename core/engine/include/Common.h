1f64f3a118e7505b0abb1134c716a7d3f5dcb2a5
Eugene <sudakovzhenya02@gmail.com>
2025-04-01 01:19:14 +0700

    [refactor]  Реорганизация определений дял Engine

---
+++ core/engine/include/Common.h
@@ -0,0 +1,24 @@
+#pragma once 
+
+// GLM includes
+#include <glm/glm.hpp>
+#include <glm/gtc/quaternion.hpp>
+
+constexpr float MIN_FPS                 = 30.0f;
+constexpr float TARGET_FPS              = 60.0f;
+constexpr float MAX_FPS                 = 140.f;
+
+constexpr float MIN_PHYSICS_STEP        = 1.f / MAX_FPS;    
+constexpr float TARGET_PHYSICS_STEP     = 1.f / TARGET_FPS; 
+constexpr float MAX_PHYSICS_STEP        = 1.f / MIN_FPS;
+
+constexpr float MAX_DELTA_TIME          = 0.05f;
+
+
+namespace Type {
+
+    using Vector3D      = glm::vec3;
+    using Scale3D       = glm::vec3;
+    using Quaternion    = glm::quat;
+
+} // namespace Type
