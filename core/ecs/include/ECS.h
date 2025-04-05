449d30e47b56985f7e9a029a175d90280b7219e2
Eugene <sudakovzhenya02@gmail.com>
2025-04-05 15:26:34 +0700

    [feat] Интерфейс для удобного импорта
    - Добавил файл включающий в себя необходимые заголовочные файлы для выстраивания ECS в проекте.

---
+++ core/ecs/include/ECS.h
@@ -0,0 +1,9 @@
+#pragma once
+
+/* base ECS design includes */
+#include <base/Component.h>
+#include <base/Entity.h>
+#include <base/System.h>
+
+/* utils ECS design include */
+#include <utils/ECSManager.hpp>
