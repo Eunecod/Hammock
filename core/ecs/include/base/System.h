cc8ce61bc1538a8a692638e570d3387aeb338edc
Eugene <sudakovzhenya02@gmail.com>
2025-04-09 14:53:49 +0700

    [feat] Обновление поведения и отвественности абстракции System
    - Добавленн конструктор и ecs для явного определения его в наследниках.

--- core/ecs/include/base/System.h
+++ core/ecs/include/base/System.h
@@ -1,9 +1,15 @@
 #pragma once
 
+#include <utils/ECSManager.hpp>
+
 
 class System {
 public:
+    explicit System(ECSManager& ecs) : ecs(ecs) { };
     ~System() = default;
 
     virtual void Update(const float) = 0;
+
+protected:
+    ECSManager& ecs;
 };
