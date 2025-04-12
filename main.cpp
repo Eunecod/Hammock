9c777a3752b5519e534a52abd3b049e68c19bff6
Eugene <sudakovzhenya02@gmail.com>
2025-04-12 12:26:58 +0700

    [refactor] Новый шаблон для создания базового приложения.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
-#include <Engine.h>
 #include <Window.h>
+#include <Scene.h>
+#include <Engine.h>
 
 
 int main() {
     Hammock::Window window(800, 600, "Hammock");
     window.CreateInstance();
 
-    Hammock::Engine engine(&window);
+    Scene scene;
+
+    Hammock::Engine engine(&window, &scene);
     engine.Loop();
 
     return 0;
